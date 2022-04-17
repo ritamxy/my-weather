e79919ed2a4449c06e3b6ea3cdd56e3d0ab34f6c
ritamxy <58613546+ritamxy@users.noreply.github.com>
2022-04-17 22:03:51 +1000

    add main image

--- my-weather/src/components/Layout/Header.js
+++ my-weather/src/components/Layout/Header.js
@@ -1,12 +1,18 @@
 import classes from "./Header.module.css";
 import ThemeChangeButton from "./ThemeChangeButton";
+import snowImage from "../../assets/snow-mountain.jpg";
 
 const Header = () => {
   return (
-    <header className={classes.header}>
-      <h1>My Weather</h1>
-      <ThemeChangeButton />
-    </header>
+    <div>
+      <header className={classes.header}>
+        <h1>My Weather</h1>
+        <ThemeChangeButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={snowImage} alt="choose your weather" />
+      </div>
+    </div>
   );
 };
 
